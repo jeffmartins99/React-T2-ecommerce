bd716f8016504633e176644f77d6589c51e845fe
jeffmartins99 <54650142+jeffmartins99@users.noreply.github.com>
2019-10-31 17:20:07 -0300

    version 31-10

--- src/App.js
+++ src/App.js
@@ -38,11 +38,24 @@ class Site extends React.Component{
   }
 }
 
+class Conteudo extends React.Component{
+  renderSection(){
+    return (
+      <Section/>
+    );
+  }
+  render(){
+    return(
+      <div><this.renderSection/></div>
+    );
+  }
+}
+
 function Nav(){
   return (
     <nav class="navbar navbar-expand-lg navbar-dark ftco_navbar bg-dark ftco-navbar-light" id="ftco-navbar">
       <div class="container">
-        <a class="navbar-brand" href="index.html">Raptor.</a>
+        <a class="navbar-brand" href="index.html">WWJ Internet</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#ftco-nav" aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
           <span class="oi oi-menu"></span> Menu
         </button>
@@ -53,7 +66,6 @@ function Nav(){
             <li class="nav-item"><a href="about.html" class="nav-link">Sobre</a></li>
             <li class="nav-item"><a href="domain.html" class="nav-link">Dominios</a></li>
             <li class="nav-item"><a class="nav-link" href="hosting.html">Servidores</a></li>
-            <li class="nav-item"><a href="blog.html" class="nav-link">Blog</a></li>
             <li class="nav-item"><a href="contact.html" class="nav-link">Contato</a></li>
           </ul>
         </div>
@@ -72,9 +84,9 @@ function Banner(){
         <div class="slider-text d-md-flex align-items-center" data-scrollax-parent="true">
         
           <div class="one-forth pr-md-4 ftco-animate0 align-self-md-center" data-scrollax=" properties: { translateY: '70%' }">
-          	<h1 class="mb-4" data-scrollax="properties: { translateY: '30%', opacity: 1.6 }" >Design. <br/> Development. <br/> Hosting.</h1>
-            <p class="mb-md-5 mb-sm-3" data-scrollax="properties: { translateY: '30%', opacity: 1.6 }">A small river named Duden flows by their place and supplies it with the necessary regelialia.</p>
-            <p data-scrollax="properties: { translateY: '30%', opacity: 1.6 }"><a href="#" class="btn btn-primary px-4 py-3">Get started</a></p>
+          	<h1 class="mb-4" data-scrollax="properties: { translateY: '30%', opacity: 1.6 }" >Design. <br/> Desenvolvimento. <br/> Host.</h1>
+            <p class="mb-md-5 mb-sm-3" data-scrollax="properties: { translateY: '30%', opacity: 1.6 }">Compre aqui seu melhor e mais estavel servidor para tudo que vc deseja.</p>
+            <p data-scrollax="properties: { translateY: '30%', opacity: 1.6 }"><a href="#" class="btn btn-primary px-4 py-3">Iniciar</a></p>
           </div>
           <div class="one-half align-self-md-end align-self-sm-center">
             <div class="slider-carousel owl-carousel">
@@ -90,94 +102,73 @@ function Banner(){
   );
 }
 
-function Conteudo(){
+class Planos extends React.Component{
+  constructor(props) {
+    super(props);
+    this.state = {
+      id: props.id,
+      tipo: props.tipo,
+      taxa: props.taxa,
+      largura: props.largura,
+      armaz: props.armaz,
+      exced: props.exced
+    };
+  }
+
+  mudaTela = () => {
+    if(this.props.id == 1){
+      this.setState({
+        tipo: "teste de mudança"
+      });
+    }
+    else{
+      this.setState({
+        tipo: "Outro Teste"
+      });
+    }
+    
+  }
+
+  render(){
+    return(
+      <div class="col-md-3 ftco-animate0">
+        <div class="block-7">
+          <div class="text-center">
+            <h2 class="heading">{this.state.tipo}</h2>
+            <span class="price"><sup>R$</sup> <span class="number">{this.state.taxa}</span></span>
+            <span class="excerpt d-block">Todos recursos estão inclusos</span>
+            <button class="btn btn-success" onClick={this.mudaTela}>Adquirir</button>
+            <h3 class="heading-2 mb-3">Aproveite todos os recursos</h3>
+            
+            <ul class="pricing-text">
+              <li><strong>{this.state.largura}GB</strong> Largura de banda</li>
+              <li><strong>{this.state.armaz}GB</strong> Armazenamento</li>
+              <li><strong>${this.state.exced}.00 / GB</strong> Excedentes</li>
+              <li>Todos os recursos</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+function Section(){
   return (
     <section class="ftco-section bg-light">
     	<div class="container">
     		<div class="row justify-content-center mb-5 pb-5">
           <div class="col-md-7 text-center heading-section ftco-animate0">
-            <span class="subheading">Pricing Plans</span>
-            <h2 class="mb-4">Our Best Pricing</h2>
+            <span class="subheading">Preços dos Planos</span>
+            <h2 class="mb-4">Nosso Melhor Preço</h2>
           </div>
         </div>
-    		<div class="row">
-	        <div class="col-md-3 ftco-animate0">
-	          <div class="block-7">
-	            <div class="text-center">
-	            <h2 class="heading">Free</h2>
-	            <span class="price"><sup>$</sup> <span class="number">0</span></span>
-	            <span class="excerpt d-block">100% free. Forever</span>
-	            <a href="#" class="btn btn-primary d-block px-3 py-3 mb-4">Get Started</a>
-	            
-	            <h3 class="heading-2 mb-3">Enjoy All The Features</h3>
-	            
-	            <ul class="pricing-text">
-	              <li><strong>150 GB</strong> Bandwidth</li>
-	              <li><strong>100 GB</strong> Storage</li>
-	              <li><strong>$1.00 / GB</strong> Overages</li>
-	              <li>All features</li>
-	            </ul>
-	            </div>
-	          </div>
-	        </div>
-	        <div class="col-md-3 ftco-animate0">
-	          <div class="block-7">
-	            <div class="text-center">
-	            <h2 class="heading">Startup</h2>
-	            <span class="price"><sup>$</sup> <span class="number">19</span></span>
-	            <span class="excerpt d-block">All features are included</span>
-	            <a href="#" class="btn btn-primary d-block px-3 py-3 mb-4">Get Started</a>
-	            
-	            <h3 class="heading-2 mb-3">Enjoy All The Features</h3>
-	            
-	            <ul class="pricing-text">
-	              <li><strong>450 GB</strong> Bandwidth</li>
-	              <li><strong>400 GB</strong> Storage</li>
-	              <li><strong>$2.00 / GB</strong> Overages</li>
-	              <li>All features</li>
-	            </ul>
-	            </div>
-	          </div>
-	        </div>
-	        <div class="col-md-3 ftco-animate0">
-	          <div class="block-7">
-	            <div class="text-center">
-	            <h2 class="heading">Premium</h2>
-	            <span class="price"><sup>$</sup> <span class="number">49</span></span>
-	            <span class="excerpt d-block">All features are included</span>
-	            <a href="#" class="btn btn-primary d-block px-3 py-3 mb-4">Get Started</a>
-	            
-	            <h3 class="heading-2 mb-3">Enjoy All The Features</h3>
-	            
-	            <ul class="pricing-text">
-	              <li><strong>250 GB</strong> Bandwidth</li>
-	              <li><strong>200 GB</strong> Storage</li>
-	              <li><strong>$5.00 / GB</strong> Overages</li>
-	              <li>All features</li>
-	            </ul>
-	            </div>
-	          </div>
-	        </div>
-	        <div class="col-md-3 ftco-animate0">
-	          <div class="block-7">
-	            <div class="text-center">
-	            <h2 class="heading">Pro</h2>
-	            <span class="price"><sup>$</sup> <span class="number">99</span></span>
-	            <span class="excerpt d-block">All features are included</span>
-	            <a href="#" class="btn btn-primary d-block px-3 py-3 mb-4">Get Started</a>
-	            
-	            <h3 class="heading-2 mb-3">Enjoy All The Features</h3>
-	            
-	            <ul class="pricing-text">
-	              <li><strong>450 GB</strong> Bandwidth</li>
-	              <li><strong>400 GB</strong> Storage</li>
-	              <li><strong>$20.00 / GB</strong> Overages</li>
-	              <li>All features</li>
-	            </ul>
-	            </div>
-	          </div>
-	        </div>
-	      </div>
+    		  <div class="row">
+            <Planos id="1" tipo="Gratis" taxa="0" largura="50" armaz="100" exced="20"/>
+            <Planos id="2" tipo="Basico" taxa="19" largura="150" armaz="200" exced="5"/>
+            <Planos id="3" tipo="Premium" taxa="49" largura="250" armaz="300" exced="2"/>
+            <Planos id="4" tipo="Pro" taxa="99" largura="450" armaz="400" exced="1"/>
+          </div>
     	</div>
     </section>
   );
@@ -190,21 +181,20 @@ function Rodape(){
         <div class="row mb-5">
           <div class="col-md">
             <div class="ftco-footer-widget mb-4">
-              <h2 class="ftco-heading-2">Raptor.</h2>
-              <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
-              <p class="mt-4"><a href="#" class="btn btn-primary p-3">Get in touch</a></p>
+              <h2 class="ftco-heading-2">WWJ Internet</h2>
+              <p>Compre aqui seu melhor e mais estavel servidor para tudo que vc deseja.</p>
+              <p class="mt-4"><a href="#" class="btn btn-primary p-3">Entre em contato</a></p>
             </div>
           </div>
           <div class="col-md">
             <div class="ftco-footer-widget mb-4 ml-md-5">
-              <h2 class="ftco-heading-2">Unseful Links</h2>
+              <h2 class="ftco-heading-2">Links Úteis</h2>
               <ul class="list-unstyled">
                 <li><a href="#" class="py-2 d-block">Servers</a></li>
-                <li><a href="#" class="py-2 d-block">Windos Hosting</a></li>
+                <li><a href="#" class="py-2 d-block">Windows Hosting</a></li>
                 <li><a href="#" class="py-2 d-block">Cloud Hosting</a></li>
                 <li><a href="#" class="py-2 d-block">OS Servers</a></li>
                 <li><a href="#" class="py-2 d-block">Linux Servers</a></li>
-                <li><a href="#" class="py-2 d-block">Policy</a></li>
               </ul>
             </div>
           </div>
@@ -212,12 +202,11 @@ function Rodape(){
              <div class="ftco-footer-widget mb-4">
               <h2 class="ftco-heading-2">Navigational</h2>
               <ul class="list-unstyled">
-                <li><a href="#" class="py-2 d-block">Home</a></li>
-                <li><a href="#" class="py-2 d-block">Domain</a></li>
-                <li><a href="#" class="py-2 d-block">Hosting</a></li>
-                <li><a href="#" class="py-2 d-block">About</a></li>
-                <li><a href="#" class="py-2 d-block">Blog</a></li>
-                <li><a href="#" class="py-2 d-block">Contact</a></li>
+                <li><a href="#" class="py-2 d-block">Inicio</a></li>
+                <li><a href="#" class="py-2 d-block">Dominio</a></li>
+                <li><a href="#" class="py-2 d-block">Servidores</a></li>
+                <li><a href="#" class="py-2 d-block">Sobre</a></li>
+                <li><a href="#" class="py-2 d-block">Contato</a></li>
               </ul>
             </div>
           </div>
@@ -225,8 +214,8 @@ function Rodape(){
             <div class="ftco-footer-widget mb-4">
             	<div class="block-23 mb-3">
 	              <ul>
-	                <li><span class="icon icon-map-marker"></span><span class="text">203 Fake St. Mountain View, San Francisco, California, USA</span></li>
-	                <li><a href="#"><span class="icon icon-phone"></span><span class="text">+2 392 3929 210</span></a></li>
+	                <li><span class="icon icon-map-marker"></span><span class="text">203 Fake St. JD. Aeroporto, Lins, São Paulo, Brasil</span></li>
+	                <li><a href="#"><span class="icon icon-phone"></span><span class="text">+55 14 3929 210</span></a></li>
 	                <li><a href="#"><span class="icon icon-envelope"></span><span class="text">info@yourdomain.com</span></a></li>
 	                <li><span class="icon icon-clock-o"></span><span class="text">Saturday &mdash; Sunday 8:00am - 5:00pm</span></li>
 	              </ul>
