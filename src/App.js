a1a9b27af336052107dfa3a462dafe9cad9499c0
jeffmartins99 <54650142+jeffmartins99@users.noreply.github.com>
2019-11-25 00:22:00 -0300

    25-11-2019

--- src/App.js
+++ src/App.js
@@ -40,7 +40,7 @@ class Site extends React.Component{
         <div><this.renderNav/></div>
         <div><this.renderBanner/></div>
         <div><this.renderConteudo/></div>
-        <div><this.renderCarrinho/></div>
+        {/*<div><this.renderCarrinho/></div>*/}
         <div><this.renderRodape/></div>
       </div>
     )
@@ -112,94 +112,162 @@ function Banner(){
   );
 }
 
+const arrObj = [
+  {id:"1", tipo:"Gratis", taxa:0, largura:"50", armaz:"100", exced:"20", qtd:1, status:"1"},
+  {id:"2", tipo:"Basico", taxa:19, largura:"150", armaz:"200", exced:"5", qtd:1, status:"1"},
+  {id:"3", tipo:"Premium", taxa:49, largura:"250", armaz:"300", exced:"2", qtd:1, status:"1"},
+  {id:"4", tipo:"Pro", taxa:99, largura:"450", armaz:"400", exced:"1", qtd:1, status:"1"},
+]
+
 class Planos extends React.Component{
   constructor(props) {
     super(props);
     this.state = {
-      id: props.id,
-      tipo: props.tipo,
-      taxa: props.taxa,
-      largura: props.largura,
-      armaz: props.armaz,
-      exced: props.exced,
-      status: props.status,
-      qtd: props.qtd
+      arrObj,
+      total: 0,
+      qtd: 0,
+      tempo: 'Ano'
     };
-    this.renderProdutos = this.renderProdutos.bind(this)
+
+    this.adquire = this.adquire.bind(this);
+    this.incrementa = this.incrementa.bind(this);
+    this.alerta = this.alerta.bind(this);
   }
 
-  mudaTela = () => {
-    let cont;
-    this.setState({
-      status: 0,
-      qtd: 1
-    });  
+  adquire(key){
+    arrObj[key].status = "0";
+    this.state.total += arrObj[key].taxa;
+    //arrObj[key].qtd = 1;
+    this.setState({arrObj});
   }
 
-  renderProdutos(){
-    return(
-      
-      <div class="block-7" key={this.state.id}>
-        <div class="text-center">
-          <h2 class="heading">{this.state.tipo}</h2>
-          <span class="price"><sup>R$</sup> <span class="number">{this.state.taxa}</span></span>
-          <span class="excerpt d-block">Todos recursos estão inclusos</span>
-          <button class="btn btn-success" onClick={this.mudaTela}>Adquirir</button>
-          <h3 class="heading-2 mb-3">Aproveite todos os recursos</h3>
-          
-          <ul class="pricing-text">
-            <li><strong>{this.state.largura}GB</strong> Largura de banda</li>
-            <li><strong>{this.state.armaz}GB</strong> Armazenamento</li>
-            <li><strong>${this.state.exced}.00 / GB</strong> Excedentes</li>
-            <li>Todos os recursos</li>
-          </ul>
-        </div>
-      </div>
-      
-    );
+  incrementa(key){
+    arrObj[key].qtd += 1;
+    this.state.total += arrObj[key].taxa;
+    if(arrObj[key].qtd == 1)
+      this.state.tempo = 'Ano';
+    else
+      this.state.tempo = 'Anos';
+    this.setState({arrObj});
+  }
+
+  decrementa(key){
+    arrObj[key].qtd -= 1;
+    this.state.total -= arrObj[key].taxa;
+    if(arrObj[key].qtd == 1)
+      this.state.tempo = 'Ano';
+    else
+      this.state.tempo = 'Anos';
+    if(arrObj[key].qtd == 0){
+      arrObj[key].status = "1";
+      arrObj[key].qtd = "1";
+    }
+    this.setState({arrObj});
+  }
+
+  alerta(){
+    arrObj[0].qtd = 1;
+    arrObj[1].qtd = 1;
+    arrObj[2].qtd = 1;
+    arrObj[3].qtd = 1;
+    arrObj[0].status = 1;
+    arrObj[1].status = 1;
+    arrObj[2].status = 1;
+    arrObj[3].status = 1;
+    this.state.total = 0;
+    this.setState({arrObj});
+    alert("Comprado!!");
   }
 
   render(){
     return (
-      <div class="col-md-3 ftco-animate0">
-        <div class="block-7">
-          <div class="text-center">
-            <h2 class="heading">{this.state.tipo}</h2>
-            <span class="price"><sup>R$</sup> <span class="number">{this.state.taxa}</span></span>
-            <span class="excerpt d-block">Todos recursos estão inclusos</span>
-            <button class="btn btn-success" onClick={this.mudaTela}>Adquirir</button>
-            <h3 class="heading-2 mb-3">Aproveite todos os recursos</h3>
-            
-            <ul class="pricing-text">
-              <li><strong>{this.state.largura}GB</strong> Largura de banda</li>
-              <li><strong>{this.state.armaz}GB</strong> Armazenamento</li>
-              <li><strong>${this.state.exced}.00 / GB</strong> Excedentes</li>
-              <li>Todos os recursos</li>
-            </ul>
-          </div>
-            {
-              this.state.status == 1 ?//fazendo um if
-                <div>
-                  {/* Faz nada */}
+      <div class="row">
+        {
+          this.state.arrObj.map((item)=>{
+            return(
+              <div class="col-md-3 ftco-animate0" key={item.id}>
+                <div class="block-7">
+                  <div class="text-center">
+                    <h2 class="heading">{item.tipo}</h2>
+                    <span class="price"><sup>R$</sup> <span class="number">{item.taxa}</span></span>
+                    <span class="excerpt d-block">Todos recursos estão inclusos</span>
+                    <button class="btn btn-success" onClick={() => this.adquire(item.id-1)}>Adquirir</button>
+                    <h3 class="heading-2 mb-3">Aproveite todos os recursos</h3>
+                    
+                    <ul class="pricing-text">
+                      <li><strong>{item.largura}GB</strong> Largura de banda</li>
+                      <li><strong>{item.armaz}GB</strong> Armazenamento</li>
+                      <li><strong>${item.exced}.00 / GB</strong> Excedentes</li>
+                      <li>Todos os recursos</li>
+                    </ul>
+                  </div>
+                    {
+                      item.status == 1 ?//fazendo um if
+                        <div>
+                          {/* Faz nada */}
+                        </div>
+                      ://fazendo um else
+                        <div class="text-center">
+                          <h2 class="heading" >Adquirido</h2>
+                        </div>
+                    }
                 </div>
-              ://fazendo um else
-                <div class="text-center">
-                  <h2 class="heading" >Adquirido</h2>
-                </div>
-            }
-        </div>
-        
+              </div>
+            );
+          })
+        }
+        {
+        this.state.total != 0 ?
+          <section class="ftco-section bg-secondary">
+            <div class="container">
+            <div class="quadrado">
+              <div class="card bg-light mb-3">
+                <div class="card-header heading">Carrinhos de Compras</div>
+                  {
+                    this.state.arrObj.map((item)=>{
+                      return (
+                        <div>
+                          {
+                            item.status == 0 ? 
+                              <div>
+                                <div class="card-body">
+                                  <h5 class="card-title">{item.tipo}</h5>
+                                  <label>{item.tipo} </label><label class="direita"> 
+                                    <button class="btn" onClick={() => this.incrementa(item.id-1)}>+</button> {item.qtd+" "+this.state.tempo} 
+                                    <button class="btn" onClick={() => this.decrementa(item.id-1)}>-</button>  - Valor unitario: R$ {item.taxa},00 </label>
+                                  <p class="card-text branco">Teste Plano / Tempo / Valor / Remover / fjkhgfjhgfgsgfdsgfdsmghgfdhgfdgh</p>
+                                </div>
+                                
+                              </div> 
+                            : 
+                              <div>
+                                
+                              </div> 
+                          }
+                        </div>
+                      );
+                    })
+                  }
+                  {
+                  this.state.total != 0 ?
+                    <div class="card-footer">
+                      <small class="text-muted">Total  </small><small class="direita">R$ {this.state.total},00</small>
+                      <button class="direita btn btn-success" onClick={() => this.alerta()}>Comprar</button>
+                    </div>
+                  :
+                    <div></div>
+                  }
+              </div>
+            </div>
+            </div>
+          </section>
+        :
+          <div></div>
+        }
       </div>
     );
   }
 }
-
-const arrObj = [
-  {id:"1", tipo:"Gratis", taxa:"0", largura:"50", armaz:"100", exced:"20", qtd:"0", status:"1"},
-  {id:"2", tipo:"Basico", taxa:"19", largura:"150", armaz:"200", exced:"5", qtd:"0", status:"1"},
-  {id:"3", tipo:"Premium", taxa:"49", largura:"250", armaz:"300", exced:"2", qtd:"0", status:"1"},
-  {id:"4", tipo:"Pro", taxa:"99", largura:"450", armaz:"400", exced:"1", qtd:"0", status:"1"},
-]
 
 class Section extends React.Component{
   constructor(props) {
@@ -219,13 +287,7 @@ class Section extends React.Component{
               <h2 class="mb-4">Nosso Melhor Preço</h2>
             </div>
           </div>
-            <div class="row">
-              {this.state.arrObj.map((item)=>{
-                return(
-                  <Planos id={item.id} tipo={item.tipo} taxa={item.taxa} largura={item.largura} armaz={item.armaz} exced={item.exced} status={item.status}/>
-                );
-              })}
-            </div>
+            <Planos/>
         </div>
       </section>
     );
@@ -255,7 +317,7 @@ class Carrinho extends React.Component{
                           <div>
                             <div class="card-body">
                               <h5 class="card-title">{item.tipo}</h5>
-                              <label>{item.tipo} </label><label class="direita"> {item.qtd}Tempo / {item.taxa} </label>
+                              <label>{item.tipo} </label><label class="direita"> {item.qtd} Tempo / R$ {item.taxa},00 </label>
                               <p class="card-text branco">Tipo Plano / Tempo / Valor / Remover / fjkhgfjhgfgsgfdsgfdsmghgfdhgfdgh</p>
                             </div>
                             <div class="card-footer">
@@ -319,10 +381,10 @@ function Rodape(){
             <div class="ftco-footer-widget mb-4">
             	<div class="block-23 mb-3">
 	              <ul>
-	                <li><span class="icon icon-map-marker"></span><span class="text">203 Fake St. JD. Aeroporto, Lins, São Paulo, Brasil</span></li>
-	                <li><a href="#"><span class="icon icon-phone"></span><span class="text">+55 14 3929 210</span></a></li>
-	                <li><a href="#"><span class="icon icon-envelope"></span><span class="text">info@yourdomain.com</span></a></li>
-	                <li><span class="icon icon-clock-o"></span><span class="text">Saturday &mdash; Sunday 8:00am - 5:00pm</span></li>
+	                <li><span class="icon icon-map-marker"></span><span class="text">Unilins. JD. Aeroporto, Lins, São Paulo, Brasil</span></li>
+	                <li><a href="#"><span class="icon icon-phone"></span><span class="text">+55 14 9999 9999</span></a></li>
+	                <li><a href="#"><span class="icon icon-envelope"></span><span class="text">info@wwj.com</span></a></li>
+	                
 	              </ul>
 	            </div>
               <ul class="ftco-footer-social list-unstyled float-md-left float-lft mt-5">
